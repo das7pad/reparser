5df7cbe3733c84d764f985808aa3013e3aa41494
Jakob Ackermann <das7pad@outlook.com>
2018-12-21 14:54:04 +0100

    Revert "[reparser] ensure a present end pattern when matching the start"
    This reverts commit 6523b958d02bd05be338b55ef67512303b7c1ffc.

--- reparser/__init__.py
+++ reparser/__init__.py
@@ -78,19 +78,15 @@ class Token:
     ):
         self.name = name
         self.group_start = '{}_start'.format(self.name)
+        self.pattern_start = self.modify_pattern(
+            pattern=pattern_start,
+            group=self.group_start,
+        )
         if not pattern_end:
             self.group_end = None
-            self.pattern_start = self.modify_pattern(
-                pattern=pattern_start,
-                group=self.group_start,
-            )
             self.pattern_end = None
         else:
             self.group_end = '{}_end'.format(self.name)
-            self.pattern_start = self.modify_pattern(
-                pattern=pattern_start + '(?=.+?%s)' % pattern_end,
-                group=self.group_start,
-            )
             self.pattern_end = self.modify_pattern(pattern_end, self.group_end)
         self.skip = skip
         self.params = params
